eb1e21ebd3b03765744c5c17f1d38b1481e99cb5
Zadios <arielviscovich@gmail.com>
2025-11-12 12:50:18 -0300

    Add repository layer

---
+++ src/main/java/com/viscovich/to_do_list/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.viscovich.to_do_list.repository;
+
+import com.viscovich.to_do_list.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
